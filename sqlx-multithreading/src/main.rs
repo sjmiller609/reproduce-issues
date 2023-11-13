8e701424f97b7b8975347fa044c0d65b41394d92
Steven Miller <sjmiller609@gmail.com>
2023-11-13 14:46:50 -0500

    Add background threads, not reproducing issue

--- sqlx-multithreading/src/main.rs
+++ sqlx-multithreading/src/main.rs
@@ -1,7 +1,9 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
-use std::env;
+use std::{env, time};
+use std::sync::{Arc, Mutex};
+use tokio;
 
 async fn hello(db_pool: web::Data<PgPool>) -> impl Responder {
     // Execute a simple query against the database
@@ -25,6 +27,24 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to create pool");
 
+    let background_threads: Arc<Mutex<Vec<tokio::task::JoinHandle<()>>>> =
+        Arc::new(Mutex::new(Vec::new()));
+
+    let db_pool_clone = db_pool.clone();
+    let bg_thread = tokio::spawn(async move {
+        loop {
+            match sqlx::query!("SELECT 1 as background")
+                .fetch_one(&db_pool_clone)
+                .await
+            {
+                Ok(_) => println!("Background query executed successfully."),
+                Err(e) => eprintln!("Background query failed: {:?}", e),
+            }
+            tokio::time::sleep(time::Duration::from_secs(10)).await;
+        }
+    });
+
+    background_threads.lock().unwrap().push(bg_thread);
     // Start HTTP server
     HttpServer::new(move || {
         App::new()
